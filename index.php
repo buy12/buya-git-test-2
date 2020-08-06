e0e4c575ff20950ed6dc8e8606a1c7c2116d560b
buy12 <kurokoocchi@gmail.com>
2020-08-06 21:40:41 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet velit, sequi quae rerum placeat modi earum perspiciatis. Suscipit distinctio nisi beatae qui nihil, vero minima iste quasi similique autem iure!</p>
 </body>
 </html>
